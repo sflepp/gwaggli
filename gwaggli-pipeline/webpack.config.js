07c33a3423fa9e6890d3b3f878fa874eb8296253
Simon Flepp <simon@flepp.io>
2023-06-27 21:27:49 +0200

    Setup webpack

---
+++ gwaggli-pipeline/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+// https://medium.com/@turhan.oz/typescript-with-jasmine-easy-project-setup-530c7cc764e8
+
+module.exports = {
+    target: 'node',
+    mode: 'production',
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    externals: [nodeExternals()],
+};
